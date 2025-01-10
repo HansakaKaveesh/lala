aceaf26310b85761f0377ad6503a4008f8893b50
HansakaKaveesh <hansakakaveesh2@gmail.com>
2025-01-10 11:07:19 +0530

    Refactor Categories component for improved styling and layout consistency

--- src/app/components/home/Categories.js
+++ src/app/components/home/Categories.js
@@ -24,15 +24,18 @@ const ShopByCategory = () => {
       hoverImage: "/Tops.gif",
       link: "/categories/tops",
     },
+    
   ];
 
   const [hoveredCategory, setHoveredCategory] = useState(null);
 
   return (
-    <section className="py-20 bg-gradient-to-b from-gray-100 to-gray-200">
-      <div className="container mx-auto px-6 lg:px-16">
-      <h2 className="text-xl font-bold text-gray-800 text-center mb-12">Shop by Category</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+    <section className="py-16 bg-gradient-to-b from-gray-100 to-gray-200">
+      <div className="container mx-auto px-4 lg:px-8">
+        <h2 className="text-lg font-bold text-gray-800 text-center mb-10">
+          Shop by Category
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {categories.map((category, index) => (
             <a
               href={category.link}
@@ -41,9 +44,9 @@ const ShopByCategory = () => {
               onMouseLeave={() => setHoveredCategory(null)}
               className="transition-transform duration-300 hover:scale-105"
             >
-              <Card className="relative rounded-xl overflow-hidden shadow-lg hover:shadow-2xl">
+              <Card className="relative rounded-lg overflow-hidden shadow-md hover:shadow-lg">
                 <CardHeader className="p-0">
-                  <div className="relative h-72">
+                  <div className="relative h-56">
                     <Image
                       src={
                         hoveredCategory === index
@@ -54,11 +57,11 @@ const ShopByCategory = () => {
                       fill
                       className="object-cover w-full h-full"
                     />
-                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-60 group-hover:opacity-70 transition-opacity duration-300"></div>
+                    <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50 group-hover:opacity-60 transition-opacity duration-300"></div>
                   </div>
                 </CardHeader>
-                <CardContent className="absolute bottom-4 left-4 text-white">
-                  <h3 className="text-2xl font-bold">{category.name}</h3>
+                <CardContent className="absolute bottom-3 left-3 text-white">
+                  <h3 className="text-lg font-bold">{category.name}</h3>
                 </CardContent>
               </Card>
             </a>
